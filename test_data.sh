a5fad32bb073779711d1c1a806cbcc4709fa6de2
Maiara Rodrigues <85491832+MaiRodrigues@users.noreply.github.com>
2025-04-10 15:42:54 -0300

    Add files via upload

---
+++ test_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+cp ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+
+
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+
+
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+
+
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+
+
+for (( i=1; i <= 1000; ++i ))
+do
+    cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+    cat ~/home/maiara_sf/test_repo/data/data_test.txt >> new_test
+done
